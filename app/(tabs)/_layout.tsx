d588ff85101618bb52f321484bccc94c8e3a159c
Franki09 <fgomezrosell@gmail.com>
2025-10-15 17:25:46 -0300

    Se agrego las paginas que se van a usar junto con tabs. y la informacion que se va a consumir. Falta maquetear e estilos.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,39 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { Ionicons } from "@expo/vector-icons";
+import Feather from "@expo/vector-icons/Feather";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: true,
+        headerShown: true,
+        tabBarActiveTintColor: "green",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          title: "Favoritos",
+          tabBarIcon: ({ color, size }) => <Ionicons name="heart" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "Sobre la App",
+          tabBarIcon: ({ color, size }) => <Feather name="info" size={size} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,11 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </SafeAreaProvider>
+  );
 }
