3515d3c0521f5e38d311ae370a6790e0b2799137
Franki09 <fgomezrosell@gmail.com>
2025-10-16 19:19:09 -0300

    Se maqueteo el home y el about. Falta el detail y favorites. Ademas se agregaron colores personalizados.

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,43 @@
+import { FlatList, Text, View } from "react-native";
+import { spotData } from "@/seeders/spotData.js";
+import { Image } from "expo-image";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-black p-5">
+      <FlatList
+        data={spotData}
+        numColumns={2}
+        keyExtractor={(item) => item.id.toString()}
+        columnWrapperStyle={{ gap: 16 }}
+        contentContainerStyle={{
+          paddingTop: 20,
+          gap: 16,
+          alignItems: "center",
+        }}
+        ListHeaderComponent={() => (
+          <View className="mb-6">
+            <Text className="text-xl text-white mb-2 text-center">Bienvenido a TravelSpot 🌎</Text>
+            <Text className="text-white text-center">
+              En TravelSpot podrás descubrir destinos turísticos increíbles con toda la información que necesitas: ubicación,
+              clima, precios de hospedaje y mucho más. ¿Encontraste tu próximo destino? ¿O tal vez uno que ya visitaste y te
+              encantó? Guárdalo en favoritos para planificar tu viaje más tarde.
+            </Text>
+          </View>
+        )}
+        renderItem={({ item }) => (
+          <Link href={""}>
+            <View className="bg-red-500 w-40 h-full flex-1 rounded-lg overflow-hidden">
+              <Image source={{ uri: item.image }} style={{ width: 160, height: 150 }} />
+              <View className="p-2">
+                <Text className="text-white font-semibold">{item.name}</Text>
+                <Text className="text-white text-xs">{item.address}</Text>
+              </View>
+            </View>
+          </Link>
+        )}
+      />
+    </View>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,30 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          dark: "#0B0B0B",
+          DEFAULT: "#121212",
+          light: "#1A1A1A",
+        },
+        green: {
+          dark: "#064E3B",
+          DEFAULT: "#10B981",
+          light: "#6EE7B7",
+        },
+        beige: {
+          dark: "#B8A57A",
+          DEFAULT: "#D6C59A",
+          light: "#F3EED9",
+        },
+        orange: {
+          dark: "#9A3412",
+          DEFAULT: "#F97316",
+          light: "#FDBA74",
+        },
+      },
+    },
   },
   plugins: [],
 };
